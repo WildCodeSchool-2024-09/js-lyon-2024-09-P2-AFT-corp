79fe28bd318a1599cf993dcc0954849e0011d94c
fouzy chikouche <fouzy_69@hotmail.fr>
2024-11-08 10:09:06 +0100

    up

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,14 +1,17 @@
 import CORP from "../CORP.png";
+import Utilisateur from "../utilisateur.png";
+
 export default function NavBar() {
   return (
     <div className="navbar">
       <img id="logo" src={CORP} alt="" />
       <ul>
         <li>Acceuil</li>
-        <li>Film</li>
-        <li>Serie</li>
-        <input type="text" />
+        <li>Films</li>
+        <li>Série</li>
       </ul>
+      <input type="text" />
+      <img id="avatar" src={Utilisateur} alt="" />
       <nav />
     </div>
   );
